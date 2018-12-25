21df3a30a9e93930589bf97f3d462e130864be67
hck <art.hacker@ya.ru>
2018-12-25 18:24:13 +0300

    fixes

--- src/frontend/modules/Player/Route/PlayerRoute/index.ts
+++ src/frontend/modules/Player/Route/PlayerRoute/index.ts
@@ -1,7 +1,7 @@
 import {Component, Inject, LOCALE_ID} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 import {Player} from "../../Entity/Player";
-import {formatDate} from "@angular/common";
+// import {formatDate} from "@angular/common";
 import {GameByPlayerResponse} from "../../../Game/Http/GameByPlayerResponse";
 
 @Component({
@@ -22,22 +22,22 @@ export class PlayerRoute {
             this.player = data.player;
             this.playerGames = data.playerGames;
 
-            this.playerArray = [
-                {"field" : "First name", "value" : this.player.first_name},
-                {"field" : "Last name", "value" : this.player.last_name},
-                {"field" : "Native name", "value" : this.player.native_name},
-                {"field" : "Birthday", "value" : formatDate(this.player.birthday, 'yyyy-MM-dd', this.locale)},
-                {"field" : "Birth Place", "value" : this.player.birthPlace},
-                {"field" : "Height", "value" : this.player.height + "cm"},
-                {"field" : "Number", "value" : this.player.number},
-                {"field" : "Contract until", "value" : formatDate(this.player.contract_until, 'yyyy-MM-dd', this.locale) },
-                {"field" : "Twitter", "value" : this.player.twitter},
-                {"field" : "Facebook", "value" : this.player.facebook},
-                {"field" : "Instagram", "value" : this.player.instagram},
-                {"field" : "Agents", "value" : this.player.agents}
-            ]
-                .filter(item => !!item.value)
-            ;
+            // this.playerArray = [
+            //     {"field" : "First name", "value" : this.player.first_name},
+            //     {"field" : "Last name", "value" : this.player.last_name},
+            //     {"field" : "Native name", "value" : this.player.native_name},
+            //     {"field" : "Birthday", "value" : formatDate(this.player.birthday, 'yyyy-MM-dd', this.locale)},
+            //     {"field" : "Birth Place", "value" : this.player.birthPlace},
+            //     {"field" : "Height", "value" : this.player.height + "cm"},
+            //     {"field" : "Number", "value" : this.player.number},
+            //     {"field" : "Contract until", "value" : formatDate(this.player.contract_until, 'yyyy-MM-dd', this.locale) },
+            //     {"field" : "Twitter", "value" : this.player.twitter},
+            //     {"field" : "Facebook", "value" : this.player.facebook},
+            //     {"field" : "Instagram", "value" : this.player.instagram},
+            //     {"field" : "Agents", "value" : this.player.agents}
+            // ]
+            //     .filter(item => !!item.value)
+            // ;
         });
     }
 }
